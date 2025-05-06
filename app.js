5ef47409321cc7a9ec2ba1441f811d1d498e3fb1
Büşra Sude Külek <bkulek@deloitte.com>
2025-05-06 22:54:31 +0300

    theme toggle button updated

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const body = document.body;
 const toggleButton = document.getElementById('toggle-theme');
 
 toggleButton.addEventListener('click', function() {
-    body.classList.remove('light-mode');
-    body.classList.add('dark-mode');
+    if (body.classList.contains('dark-mode')) {
+        body.classList.remove('dark-mode');
+        body.classList.add('light-mode');
+    } else {
+        body.classList.remove('light-mode');
+        body.classList.add('dark-mode');
+    }
 });
